a3018a0cd7100abbfa625288822fc7646864abb6
Gerardo Ballesteros <gballesteros@M-GBALLESTEROS.local>
2023-11-06 22:22:48 -0300

    feature/link-reclamos

--- src/app/components/Pages/Informacion/Horarios.jsx
+++ src/app/components/Pages/Informacion/Horarios.jsx
@@ -14,7 +14,7 @@ export const Horarios = () => {
                 <h1><strong>Lunes a jueves: 09:30 hrs - 18:000hrs</strong></h1>
                 <h1><strong>Viernes: 09:30 hrs - 16:000hrs</strong></h1>
                 <br />
-                <h1>También puedes acceder al formulario de: <a style={{ 'color': 'blue' }} href="https://forms.gle/iFwuX2jY3KKmsEfL6" target="_blank">reclamos, sugerencias o felicitaciones.</a></h1>
+                <h1>También puedes acceder al formulario de:  <a style={{ 'color': 'blue' }} href="https://forms.gle/iFwuX2jY3KKmsEfL6" target="_blank">reclamos, sugerencias o felicitaciones.</a></h1>
             </div>
         </>
     )
